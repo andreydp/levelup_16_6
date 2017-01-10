df9a782a5b3b5890e3d19c28abb183d2cdade05c
andreydp <poletaiev@gmail.com>
2017-01-10 21:03:10 +0200

    class commit

---
+++ src/ua/dp/levelup/addressbook/entity/Citizen.java
@@ -0,0 +1,92 @@
+package ua.dp.levelup.addressbook.entity;
+
+/**
+ * Created by java on 10.01.2017.
+ */
+public class Citizen
+{
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private long streetId;
+
+    public Citizen(String firstName, String lastName, int age, long streetId)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.streetId = streetId;
+    }
+
+    public Citizen(Long id, String firstName, String lastName, int age, long streetId)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.streetId = streetId;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public long getStreetId()
+    {
+        return streetId;
+    }
+
+    public void setStreetId(long streetId)
+    {
+        this.streetId = streetId;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Citizen{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", streetId=" + streetId +
+                '}';
+    }
+}
