4dc8c2b472cff73d9c74b0c2633a5bcbaeb8e1a1
andreydp <poletaiev@gmail.com>
2017-01-24 20:46:08 +0200

    class commit

---
+++ src/ua/dp/levelup/classwork/lesson20/Node.java
@@ -0,0 +1,35 @@
+package ua.dp.levelup.classwork.lesson20;
+
+/**
+ * Created by java on 24.01.2017.
+ */
+public class Node
+{
+   private Node next = null;
+   private Object value;
+
+    public Node(Object value)
+    {
+        this.value = value;
+    }
+
+    public Node next()
+    {
+        return next;
+    }
+
+    public void setNext(Node next)
+    {
+        this.next = next;
+    }
+
+    public Object getValue()
+    {
+        return value;
+    }
+
+    public void setValue(Object value)
+    {
+        this.value = value;
+    }
+}
