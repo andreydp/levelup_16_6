51016a70e1119d6136cb4ad13751f2ea438e7a92
andreydp <poletaiev@gmail.com>
2017-01-25 23:10:04 +0200

    HW 20

---
+++ src/ua/dp/levelup/homework/lesson20/DoubleLinkedList.java
@@ -0,0 +1,75 @@
+package ua.dp.levelup.homework.lesson20;
+
+import java.util.Optional;
+
+/**
+ * Created by andrey on 25.01.17.
+ */
+public class DoubleLinkedList extends AbstractList
+{
+    @Override
+    public void addNode(Node node, int index)
+    {
+
+    }
+
+    @Override
+    public void remove(int index)
+    {
+
+    }
+
+    @Override
+    public void addFirst(Node node)
+    {
+
+    }
+
+    @Override
+    public void addLast(Node node)
+    {
+
+    }
+
+    @Override
+    public void removeFirst()
+    {
+
+    }
+
+    @Override
+    public void removeLast()
+    {
+
+    }
+
+    @Override
+    public Optional<Node> getFirst()
+    {
+        return null;
+    }
+
+    @Override
+    public Optional<Node> getLast()
+    {
+        return null;
+    }
+
+    @Override
+    public Optional<Node> get(int index)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isNotEmpty()
+    {
+        return false;
+    }
+}
