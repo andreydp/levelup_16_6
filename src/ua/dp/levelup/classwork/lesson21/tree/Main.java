3e841923295add61b6dcf18f7b682d5fa608f9e1
andreydp <poletaiev@gmail.com>
2017-01-27 21:02:00 +0200

    class commit

---
+++ src/ua/dp/levelup/classwork/lesson21/tree/Main.java
@@ -0,0 +1,28 @@
+package ua.dp.levelup.classwork.lesson21.tree;
+
+import java.util.Comparator;
+
+/**
+ * Created by java on 27.01.2017.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        BinaryTree<Integer> tree = new BinaryTree<>(Integer::compare);
+        tree.add(52);
+
+        tree.add(67); //right
+        tree.add(64); //left
+        tree.add(58); //left
+
+        tree.add(29); //left
+        tree.add(1); //left
+        tree.add(1); //node
+
+        tree.add(14); //left
+        tree.add(37); //right
+
+        tree.print();
+    }
+}
